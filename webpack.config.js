36324ed557e417ad748591200ab4b289d4ceb848
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-26 23:08:03 +0300

    fix: libraryTarget: 'umd'

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,11 @@ const webpack = require('webpack');
 module.exports = {
 	devtool: 'source-map',
 	entry: './src/index',
+	externals,
 	output: {
 		path: `${__dirname}/bundle`,
-		filename: "matreshka-binders-file.min.js"
+		filename: 'matreshka-binders-file.min.js',
+		libraryTarget: 'umd'
 	},
 	module: {
 		loaders: [
@@ -19,6 +21,10 @@ module.exports = {
 	},
 
 	plugins: [
-
+		new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
 	]
 };
